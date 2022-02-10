70a70fc57e05e6721751883d6f66d24df7456e7c
Ethan Dawkins <s213634@students.aie.edu.au>
2022-02-10 15:27:40 +1100

    Collision with boxes works + 4 levels

--- golfGame/Program.cs
+++ golfGame/Program.cs
@@ -134,26 +134,33 @@ namespace golfGame
             b.storedSpeed = Math.Clamp(b.storedSpeed, 0, 25); // clamp stored speed, min and max
 
             // bounce off screen edges
-            if (b.pos.X < 0 + b.radius) ball.dir.X *= -1;
-            if (b.pos.X > windowWidth - b.radius) ball.dir.X *= -1;
-            if (b.pos.Y < 0 + 75 + b.radius) ball.dir.Y *= -1;
-            if (b.pos.Y > windowHeight - b.radius) ball.dir.Y *= -1;
+            if (b.pos.X < 0 + ball.radius) ball.dir.X = MathF.Abs(ball.dir.X);
+            if (b.pos.X > windowWidth - ball.radius) ball.dir.X = -MathF.Abs(ball.dir.X);
+            if (b.pos.Y < 0 + 75 + ball.radius) ball.dir.Y = MathF.Abs(ball.dir.Y);
+            if (b.pos.Y > windowHeight - 75 - ball.radius) ball.dir.Y = -MathF.Abs(ball.dir.Y);
         }
 
         void HoleCollision(Hole h, Ball b)
         {
             if (Vector2.Distance(b.pos, h.pos) < h.radius) // ball close enough to hole
             {
-                if (curShot < bestShot)
+                if (curShot < bestShot || bestShot == 0) // changes high score for that hole
                 {
                     string file = Directory.GetParent(Directory.GetParent(Directory.GetParent(Environment.CurrentDirectory).ToString()).ToString()) + @"\Assets\bestScores.txt";
 
                     if (File.Exists(file)) // write to best scores file
                     {
-                        bestScores[hole.holeNum - 1] = curShot.ToString();
+                        if (h.holeNum < 9) bestScores[h.holeNum - 1] = curShot.ToString();
                         File.WriteAllLines(file, bestScores);
                     }
                 }
+
+                b.speed = 0;
+                b.pos = new Vector2(windowWidth / 2, windowHeight * 0.75f);
+                ball.dir = new Vector2(0, -1);
+                if (h.holeNum < 9) h.holeNum += 1;
+                else h.holeNum = 1;
+                curShot = 0;
             }
         }
 
@@ -176,11 +183,16 @@ namespace golfGame
             Raylib.DrawRectangleGradientH((windowWidth / 2) - 150, 25, 300, 25, Color.YELLOW, Color.RED);
             RaylibExt.DrawTexture(arrowHead, ((windowWidth/2)-150) + (ball.storedSpeed * 11.15f), 45, 15, 15, Color.WHITE, 0, 0, 0);
 
+            // level name
+            Raylib.DrawRectangle(0, windowHeight - 75, windowWidth, 75, Color.DARKBLUE);
+            Raylib.DrawRectangle(0, windowHeight - 75, windowWidth, 5, Color.BLACK);
+            RaylibExt.centerText("Level - " + hole.holeNum, 40, new Vector2(windowWidth / 2, windowHeight - 55), Color.WHITE);
+
             // stroke and best stroke text
-            RaylibExt.centerText("Stroke", 25, new Vector2(windowWidth / 8, 15), Color.BLACK);
-            RaylibExt.centerText(curShot.ToString(), 30, new Vector2(windowWidth / 8, 40), Color.BLACK);
-            RaylibExt.centerText("Best", 25, new Vector2((windowWidth / 8) * 7, 15), Color.BLACK);
-            RaylibExt.centerText(bestShot.ToString(), 30, new Vector2((windowWidth / 8) * 7, 40), Color.BLACK);
+            RaylibExt.centerText("Stroke", 20, new Vector2(windowWidth / 8, 15), Color.BLACK);
+            RaylibExt.centerText(curShot.ToString(), 35, new Vector2(windowWidth / 8, 35), Color.BLACK);
+            RaylibExt.centerText("Best", 20, new Vector2((windowWidth / 8) * 7, 15), Color.BLACK);
+            RaylibExt.centerText(bestShot.ToString(), 35, new Vector2((windowWidth / 8) * 7, 35), Color.BLACK);
 
             Raylib.EndDrawing();
         }
@@ -201,19 +213,26 @@ namespace golfGame
                         string[] parts = line.Split(",");
                         RaylibExt.DrawTexture(box, windowWidth*float.Parse(parts[0]), windowHeight * float.Parse(parts[1]), float.Parse(parts[2]), float.Parse(parts[3]), Color.SKYBLUE, 0, 0.5f, 0.5f);
 
-                        float top = (windowHeight * float.Parse(parts[1])) - (float.Parse(parts[3]) / 2);
-                        float bottom = (windowHeight * float.Parse(parts[1])) + (float.Parse(parts[3]) / 2);
-                        float right = (windowWidth*float.Parse(parts[0])) + (float.Parse(parts[2]) / 2);
-                        float left = (windowWidth * float.Parse(parts[0])) - (float.Parse(parts[2]) / 2);
+                        float top = (windowHeight * float.Parse(parts[1])) - (float.Parse(parts[3]) / 2) - b.radius;
+                        float bottom = (windowHeight * float.Parse(parts[1])) + (float.Parse(parts[3]) / 2) + b.radius;
+                        float right = (windowWidth*float.Parse(parts[0])) + (float.Parse(parts[2]) / 2) + b.radius;
+                        float left = (windowWidth * float.Parse(parts[0])) - (float.Parse(parts[2]) / 2) - b.radius;
 
-                        if (b.pos.Y > top && b.pos.Y < bottom && b.pos.X > left && b.pos.X < right) // colliding with box
+                        // checks which side is the closest to the ball
+                        float[] distances = new float[4];
+                        distances[0] = Vector2.Distance(b.pos, new Vector2(windowWidth * float.Parse(parts[0]), top));
+                        distances[1] = Vector2.Distance(b.pos, new Vector2(windowWidth * float.Parse(parts[0]), bottom));
+                        distances[2] = Vector2.Distance(b.pos, new Vector2(left, windowHeight * float.Parse(parts[1])));
+                        distances[3] = Vector2.Distance(b.pos, new Vector2(right, windowHeight * float.Parse(parts[1])));
+
+                        float smallestDistance = distances.Min();
+
+                        if (b.pos.Y < bottom && b.pos.Y > top && b.pos.X < right && b.pos.X > left) // if inside collision area
                         {
-                            Vector2 dir = (new Vector2(windowWidth * float.Parse(parts[0]), windowHeight * float.Parse(parts[1])) - b.pos);
-
-                            if (dir.Y > 0) b.dir.Y *= -1;
-                            if (dir.Y < 0) b.dir.Y *= -1;
-                            if (dir.X < 0) b.dir.X *= -1;
-                            if (dir.X > 0) b.dir.X *= -1;
+                            if (smallestDistance == distances[0]) b.dir.Y = -MathF.Abs(b.dir.Y); // top
+                            if (smallestDistance == distances[1]) b.dir.Y = MathF.Abs(b.dir.Y); // bottom
+                            if (smallestDistance == distances[2]) b.dir.X = -MathF.Abs(b.dir.X); // left
+                            if (smallestDistance == distances[3]) b.dir.X = MathF.Abs(b.dir.X); // right
                         }
                     }
                 }
